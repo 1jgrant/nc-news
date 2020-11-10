35691e7946f7bdf142db7b85b926aa21ce4a11a4
james grant <grantja92@gmail.com>
2020-11-10 14:56:47 +0000

    updated exports and imports

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,7 +1,9 @@
 const { fetchTopics } = require("../models/topics");
 
-exports.getTopics = (req, res, next) => {
+getTopics = (req, res, next) => {
   fetchTopics().then((topics) => {
     res.status(200).send({ topics });
   });
 };
+
+module.exports = { getTopics };
