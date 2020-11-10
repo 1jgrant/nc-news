6d2d63556daa500e3dbf72f9e9771b433095d056
james grant <grantja92@gmail.com>
2020-11-10 15:10:44 +0000

    created send404 middleware function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const apiRouter = require('./routes/apiRouter');
+const apiRouter = require("./routes/apiRouter");
+const { send404 } = require("./controllers/errors");
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
+app.use("/*", send404);
 
 module.exports = app;
