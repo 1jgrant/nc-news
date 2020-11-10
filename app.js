59b9519cc7877b753f3bf975cd43edb591037a5d
james grant <grantja92@gmail.com>
2020-11-10 15:13:19 +0000

    created handleInternalErrors function

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const apiRouter = require("./routes/apiRouter");
-const { send404 } = require("./controllers/errors");
+const { handleInternalErrors, send404 } = require("./controllers/errors");
 
 app.use("/api", apiRouter);
 app.use("/*", send404);
+
+app.use(handleInternalErrors);
 
 module.exports = app;
--- controllers/errors.js
+++ controllers/errors.js
@@ -1,5 +1,10 @@
+const handleInternalErrors = (err, req, res, next) => {
+  console.log("unhandled internal error>>>", err);
+  res.status(500).send({ msg: "Internal Server Error" });
+};
+
 const send404 = (req, res, next) => {
   res.status(404).send({ msg: "Route not found" });
 };
 
-module.exports = { send404 };
+module.exports = { handleInternalErrors, send404 };
