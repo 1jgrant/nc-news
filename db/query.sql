f992acefdb48a9c63e732bc45ce86bbd564b807a
james grant <grantja92@gmail.com>
2020-11-11 13:03:11 +0000

    testing update syntax

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,17 @@
 \c nc_news_test
 
-SELECT articles.*, COUNT(comment_id) AS comment_count FROM articles
-LEFT JOIN comments ON articles.article_id = comments.article_id
-GROUP BY articles.article_id
+-- SELECT articles.*, COUNT(comment_id) AS comment_count FROM articles
+-- LEFT JOIN comments ON articles.article_id = comments.article_id
+-- GROUP BY articles.article_id
+BEGIN t
+SELECT articles.* FROM articles WITH (UPDLOCK)
+WHERE article_id = 1
+UPDATE articles SET votes = votes + 1
+WHERE article_id = 1
+COMMIT t
+-- UPDATE articles
+-- SET votes = votes + 1
+-- WHERE article_id = 1;
+
+-- SELECT articles.* FROM articles 
+-- WHERE article_id = 1;
