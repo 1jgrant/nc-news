804c8723e16c11e200bd33bb2ba48bd7c16b8fa1
james grant <grantja92@gmail.com>
2020-11-12 09:07:42 +0000

    testing count aggregate

--- db/query.sql
+++ db/query.sql
@@ -1,17 +1,5 @@
 \c nc_news_test
 
--- SELECT articles.*, COUNT(comment_id) AS comment_count FROM articles
--- LEFT JOIN comments ON articles.article_id = comments.article_id
--- GROUP BY articles.article_id
-BEGIN t
-SELECT articles.* FROM articles WITH (UPDLOCK)
-WHERE article_id = 1
-UPDATE articles SET votes = votes + 1
-WHERE article_id = 1
-COMMIT t
--- UPDATE articles
--- SET votes = votes + 1
--- WHERE article_id = 1;
-
--- SELECT articles.* FROM articles 
--- WHERE article_id = 1;
+SELECT articles.*, COUNT(comment_id) AS comment_count FROM articles
+LEFT JOIN comments ON articles.article_id = comments.article_id
+GROUP BY articles.article_id;
