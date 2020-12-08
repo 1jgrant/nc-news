846b407e5e59729df688368ab9a95c161c033136
james grant <grantja92@gmail.com>
2020-12-08 12:21:10 +0000

    added cors middlewae

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const apiRouter = require("./routes/apiRouter");
+const apiRouter = require('./routes/apiRouter');
 const {
   handlePSQLErrors,
   handleCustomErrors,
   handleInternalErrors,
   send404,
-} = require("./controllers/errors");
+} = require('./controllers/errors');
 
 app.use(express.json());
-app.use("/api", apiRouter);
-app.use("/*", send404);
+app.use(cors());
+app.use('/api', apiRouter);
+app.use('/*', send404);
 
 app.use(handlePSQLErrors);
 app.use(handleCustomErrors);
